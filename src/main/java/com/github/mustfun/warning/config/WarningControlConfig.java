290e1b04e4142dfda38e97e9b24824be055287f9
dengzhiyuan <dengzhiyuan@hujiang.com>
2018-03-29 18:02:25 +0800

    update: 修改config文件

--- src/main/java/com/github/mustfun/warning/config/WarningControlConfig.java
+++ src/main/java/com/github/mustfun/warning/config/WarningControlConfig.java
@@ -1,14 +1,8 @@
 package com.github.mustfun.warning.config;
 
-import com.github.mustfun.warning.processor.WarningControlContextInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
-import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -24,13 +18,7 @@ public class WarningControlConfig {
     private static Logger logger = LoggerFactory.getLogger(WarningControlConfig.class);
 
     public WarningControlConfig() {
-        logger.info("我就看看调用过没有......");
-    }
-
-    @Bean
-    @Qualifier("warningControlContextInitializer")
-    public static WarningControlContextInitializer warningControlContextInitializer() {
-        return new WarningControlContextInitializer();
+        logger.info("warning control config has bean called");
     }
 
 }
